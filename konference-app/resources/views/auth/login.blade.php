4cff946d7742da9fe0773083eb9a43518efa9fae
mariop1919 <marioperecz@gmail.com>
2024-11-21 15:08:01 +0100

    fix: updated login view

--- konference-app/resources/views/auth/login.blade.php
+++ konference-app/resources/views/auth/login.blade.php
@@ -11,17 +11,11 @@
         <div class="form-group">
             <label for="email">Email</label>
             <input type="email" name="email" class="form-control" value="{{ old('email') }}" required>
-            @error('email')
-                <small class="text-danger">{{ $message }}</small>
-            @enderror
         </div>
 
         <div class="form-group">
             <label for="password">Password</label>
             <input type="password" name="password" class="form-control" required>
-            @error('password')
-                <small class="text-danger">{{ $message }}</small>
-            @enderror
         </div>
 
         <button type="submit" class="btn btn-primary">Login</button>
