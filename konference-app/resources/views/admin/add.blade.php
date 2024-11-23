2c8f6a7c72cf7b31b9d26d4cd4852c9cc06ccbe9
peterg127 <peter.gvozdjak1@gmail.com>
2024-11-23 13:32:52 +0100

    fix: Added * to required params in admin update/add

--- konference-app/resources/views/admin/add.blade.php
+++ konference-app/resources/views/admin/add.blade.php
@@ -7,18 +7,22 @@
     <h1>Add User</h1>
     <form action="{{ route('admin.store') }}" method="POST">
         @csrf
+        
         <div class="form-group">
-            <label for="name">Name</label>
+            <label for="name">Name <span class="text-danger">*</span></label> 
             <input type="text" name="name" class="form-control" value="{{ old('name') }}" required>
         </div>
+        
         <div class="form-group">
-            <label for="email">Email</label>
+            <label for="email">Email <span class="text-danger">*</span></label> 
             <input type="email" name="email" class="form-control" value="{{ old('email') }}" required>
         </div>
+        
         <div class="form-group">
-            <label for="password">Password</label>
+            <label for="password">Password <span class="text-danger">*</span></label> 
             <input type="password" name="password" class="form-control" required>
         </div>
+        
         <button type="submit" class="btn btn-primary">Add User</button>
     </form>
 </div>
--- konference-app/resources/views/admin/edit.blade.php
+++ konference-app/resources/views/admin/edit.blade.php
@@ -8,14 +8,17 @@
     <form action="{{ route('admin.update', $user) }}" method="POST">
         @csrf
         @method('PUT')
+        
         <div class="form-group">
-            <label for="name">Name</label>
+            <label for="name">Name <span class="text-danger">*</span></label> 
             <input type="text" name="name" class="form-control" value="{{ old('name', $user->name) }}" required>
         </div>
+        
         <div class="form-group">
-            <label for="email">Email</label>
+            <label for="email">Email <span class="text-danger">*</span></label> 
             <input type="email" name="email" class="form-control" value="{{ old('email', $user->email) }}" required>
         </div>
+        
         <button type="submit" class="btn btn-primary">Update User</button>
     </form>
 </div>
